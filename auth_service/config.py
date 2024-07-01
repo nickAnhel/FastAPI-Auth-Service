4c6c550929b4dc7be1eef297b4a24ec489c28df2
nickAnhel <anhimovn1@gmail.com>
2024-07-01 19:05:46 +0300

    Added first alembic migration

--- auth_service/config.py
+++ auth_service/config.py
@@ -1,13 +1,20 @@
+import os
 from pathlib import Path
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent
 
 
-class DBConfig(BaseModel):
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR / "db.sqlite3"}"
+class DBSettings(BaseModel):
+    db_host: str | None = os.environ.get("DB_HOST")
+    db_port: str | None = os.environ.get("DB_PORT")
+    db_name: str | None = os.environ.get("DB_NAME")
+    db_user: str | None = os.environ.get("DB_USER")
+    db_password: str | None = os.environ.get("DB_PASSWORD")
+
+    db_url: str = f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     echo: bool = False
 
 
@@ -17,9 +24,7 @@ class Settings(BaseSettings):
     debug: bool
     description: str
 
-    db_settings: DBConfig = DBConfig()
-
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    db_settings: DBSettings = DBSettings()
 
 
 settings = Settings(
